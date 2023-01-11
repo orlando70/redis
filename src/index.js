5ab758ee5734f6b4d45002e43f936b380cc264e9
Chima <orlandofays3@gmail.com>
2023-01-11 23:12:09 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import DataCache from './redis.js';
+import env from 'dotenv';
+
+env.config({path: process.env.ENV_FILE_PATH});
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// A middleware function to check if the user's session is cached in Redis
+async function sessionMiddleware(req, res, next) {
+    const dataCache = new DataCache();
+    let id = "4";
+    console.log(await dataCache.getUserFromCache(id));
+    try {
+        const userData = await dataCache.getUser(id);
+        
+        req.session = userData[0];
+        next();
+    } catch (err) {
+        res.status(500).send("Error while trying to retrieve session from cache");
+    }
+}
+
+// Use the middleware in a route
+app.use("/try", sessionMiddleware, function (req, res) {
+    res.send("Welcome, " + req.session.email);
+});
+
+app.listen(5000, console.log('Server is running...'))
---
+++ src/redis.js
@@ -0,0 +1,48 @@
+import mysql from 'mysql2';
+import Redis from 'ioredis';
+
+export default class DataCache {
+    constructor() {
+        this.redis = new Redis();
+        this.connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        });
+    }
+
+    async getUserFromDB(id) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`SELECT * FROM users WHERE id = ${id}`, function (err, results) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results);
+            });
+        });
+    }
+
+    async getUserFromCache(id) {
+        const data = await this.redis.get(`data:${id}`);
+        if (data) {
+            return JSON.parse(data);
+        }
+        return null;
+    }
+
+    async storeUserInCache(id, data) {
+        this.redis.set(`data:${id}`, JSON.stringify(data));
+    }
+
+    async getUser(id) {
+        // session found in cache, send back to caller;
+        let data = await this.getUserFromCache(id);
+        if (!data) {
+            // session not found in cache, fetch from the database
+            data = await this.getUserFromDB(id);
+            await this.storeUserInCache(id, data);
+        }
+        return data;
+    }
+}
